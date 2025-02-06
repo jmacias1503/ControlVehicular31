e6249a54dc50a6d1e8485f94c897ba4ba036b01e
Alejandro Macias <alex.mac.fon@gmail.com>
2025-02-06 08:23:57 -0600

    feat: added post to post method

---
+++ Icentros_verificacion.php
@@ -0,0 +1,9 @@
+<?php
+  $idCentro = $_POST['Idcentro']
+  $idDomicilio = $_POST['IdDomicilio']
+  $nombre = $_POST['Nombre']
+
+  print("IdCentro".$idCentro."<br>")
+  print("IdDomicilio".$idDomicilio."<br>")
+  print("Nombre".$nombre."<br>")
+?>
