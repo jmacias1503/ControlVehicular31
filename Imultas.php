43026474054cff6076a75612156c99e74a5ca0a6
Alejandro Macias <alex.mac.fon@gmail.com>
2025-02-06 08:55:41 -0600

    feat: added connection get to request

---
+++ Imultas.php
@@ -0,0 +1,31 @@
+<?php
+  $FolioMultas = $_REQUEST['FolioMultas'];
+  $IdPago = $_REQUEST['IdPago'];
+  $FolioVerificacion = $_REQUEST['FolioVerificacion'];
+  $FolioCirculacion = $_REQUEST['FolioCirculacion'];
+  $IdSerie = $_REQUEST['IdSerie'];
+  $IdOficial = $_REQUEST['IdOficial'];
+  $Causa = $_REQUEST['Causa'];
+  $Observaciones = $_REQUEST['Observaciones'];
+  $Region = $_REQUEST['Region'];
+  $FechaExp = $_REQUEST['FechaExp'];
+  $Estado = $_REQUEST['Estado'];
+  $Descripcion = $_REQUEST['Descripcion'];
+  $Tipo = $_REQUEST['Tipo'];
+  $FechaHora = $_REQUEST['FechaHora'];
+
+  print("FolioMultas: ".$FolioMultas."<br>");
+  print("IdPago: ".$IdPago."<br>");
+  print("FolioVerificacion: ".$FolioVerificacion."<br>");
+  print("FolioCirculacion: ".$FolioCirculacion."<br>");
+  print("IdSerie: ".$IdSerie."<br>");
+  print("IdOficial: ".$IdOficial."<br>");
+  print("Causa: ".$Causa."<br>");
+  print("Observaciones: ".$Observaciones."<br>");
+  print("Region: ".$Region."<br>");
+  print("FechaExp: ".$FechaExp."<br>");
+  print("Estado: ".$Estado."<br>");
+  print("Descripcion: ".$Descripcion."<br>");
+  print("Tipo: ".$Tipo."<br>");
+  print("FechaHora: ".$FechaHora."<br>");
+?>
