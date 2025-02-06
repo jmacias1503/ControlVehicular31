79840bef81923c431cf6735bd32d54a5f0430195
Alejandro Macias <alex.mac.fon@gmail.com>
2025-02-06 09:06:45 -0600

    feat: added connection get to get

---
+++ ITarjetas_verificacion.php
@@ -0,0 +1,25 @@
+<?php
+  $FolioVerificacion = $_GET['FolioVerificacion'];
+  $HoraSalida = $_GET['HoraSalida'];
+  $MotivoVerificacion = $_GET['MotivoVerificacion'];
+  $FolioCertificado = $_GET['FolioCertificado'];
+  $Semestre = $_GET['Semestre'];
+  $TipoServicio = $_GET['TipoServicio'];
+  $FechaExp = $_GET['FechaExp'];
+  $HoraEntrada = $_GET['HoraEntrada'];
+  $IdCentro = $_GET['IdCentro'];
+  $IdSerie = $_GET['IdSerie'];
+  $IdPago = $_GET['IdPago'];
+
+  print("FolioVerificacion: ".$FolioVerificacion."<br>");
+  print("HoraSalida: ".$HoraSalida."<br>");
+  print("MotivoVerificacion: ".$MotivoVerificacion."<br>");
+  print("FolioCertificado: ".$FolioCertificado."<br>");
+  print("Semestre: ".$Semestre."<br>");
+  print("TipoServicio: ".$TipoServicio."<br>");
+  print("FechaExp: ".$FechaExp."<br>");
+  print("HoraEntrada: ".$HoraEntrada."<br>");
+  print("IdCentro: ".$IdCentro."<br>");
+  print("IdSerie: ".$IdSerie."<br>");
+  print("IdPago: ".$IdPago."<br>");
+?>
