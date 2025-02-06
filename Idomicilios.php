3414f35c6b46c58cf558470a555acca1a39965f0
Alejandro Macias <alex.mac.fon@gmail.com>
2025-02-06 09:09:24 -0600

    fix: merge conflicts

---
+++ Idomicilios.php
@@ -0,0 +1,19 @@
+<?php
+  $IdDomicilio = $_GET['IdDomicilio'];
+  $NumInt = $_GET['NumInt'];
+  $NumExt = $_GET['NumExt'];
+  $CP = $_GET['CP'];
+  $Estado = $_GET['Estado'];
+  $Ciudad = $_GET['Ciudad'];
+  $Colonia = $_GET['Colonia'];
+  $Calle = $_GET['Calle'];
+
+  print("IdDomicilio: ".$IdDomicilio."<br>");
+  print("NumInt: ".$NumInt."<br>");
+  print("NumExt: ".$NumExt."<br>");
+  print("CP: ".$CP."<br>");
+  print("Estado: ".$Estado."<br>");
+  print("Ciudad: ".$Ciudad."<br>");
+  print("Colonia: ".$Colonia."<br>");
+  print("Calle: ".$Calle."<br>");
+?>
