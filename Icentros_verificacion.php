d702a576b46207e81faf176b459430dfdccd1d35
David <dmataguerra@outlook.com>
2025-02-11 13:35:43 -0600

    Hola

--- Icentros_verificacion.php
+++ Icentros_verificacion.php
@@ -1,12 +1,12 @@
 <?php
-  $idCentro = $_POST['Idcentro']
-  $idDomicilio = $_POST['IdDomicilio']
-  $nombre = $_POST['Nombre']
+  $idCentro = $_POST['IdCentro'];
+  $idDomicilio = $_POST['IdDomicilio'];
+  $nombre = $_POST['Nombre'];
 
-  print("IdCentro".$idCentro."<br>")
-  print("IdDomicilio".$idDomicilio."<br>")
-  print("Nombre".$nombre."<br>")
+  print("IdCentro".$idCentro."<br>");
+  print("IdDomicilio".$idDomicilio."<br>");
+  print("Nombre".$nombre."<br>");
 
-  $SQL = "INSERT INTO centros_verificacion(Idcentro, IdDomicilio, Nombre) VALUES('$idCentro', '$idDomicilio', '$nombre')";
+  $SQL = "INSERT INTO centros_verificacion(IdCentro, IdDomicilio, Nombre) VALUES('$idCentro', '$idDomicilio', '$nombre');";
   print($SQL);
 ?>
