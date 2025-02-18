f60f59a0133a9dda56bf51a8c705d3be0632c830
Alejandro Macias <alex.mac.fon@gmail.com>
2025-02-18 14:13:23 -0600

    feat: added query to database

--- apache/app/inserts-database/Icentros_verificacion.php
+++ apache/app/inserts-database/Icentros_verificacion.php
@@ -3,10 +3,24 @@
   $idDomicilio = $_POST['IdDomicilio'];
   $nombre = $_POST['Nombre'];
 
-  print("IdCentro".$idCentro."<br>");
-  print("IdDomicilio".$idDomicilio."<br>");
-  print("Nombre".$nombre."<br>");
-
   $SQL = "INSERT INTO centros_verificacion(IdCentro, IdDomicilio, Nombre) VALUES('$idCentro', '$idDomicilio', '$nombre');";
-  print($SQL);
+
+  $host = "localhost";
+  $username = "root";
+  $password = "";
+  $database = "controlvehicular31";
+
+  $conn = mysqli_connect($host, $username, $password, $database);
+
+  if ($conn == 0) {
+    print("No se pudo conectar a la base de datos");
+  }
+  $resultSet = mysqli_query($SQL);
+  mysqli_close($conn);
+  if ($resultSet == 1){
+    print("Consulta realizada correctamente");
+  }
+  else{
+    print("Error al ejecutar la consulta: ".$resultSet->error);
+  }
 ?>
--- apache/app/inserts-database/Ipropietarios.php
+++ apache/app/inserts-database/Ipropietarios.php
@@ -2,8 +2,23 @@
   $IdPropietario = $_GET['IdPropietario'];
   $CURP = $_GET['CURP'];
 
-  print("IdPropietario: ".$IdPropietario."<br>");
-  print("CURP: ".$CURP."<br>");
   $SQL = "INSERT INTO propietarios(IdPropietario, CURP) VALUES('$IdPropietario', '$CURP');";
-  print($SQL);
+  $host = "localhost";
+  $username = "root";
+  $password = "";
+  $database = "controlvehicular31";
+
+  $conn = mysqli_connect($host, $username, $password, $database);
+
+  if ($conn == 0) {
+    print("No se pudo conectar a la base de datos");
+  }
+  $resultSet = mysqli_query($SQL);
+  mysqli_close($conn);
+  if ($resultSet == 1){
+    print("Consulta realizada correctamente");
+  }
+  else{
+    print("Error al ejecutar la consulta: ".$resultSet->error);
+  }
 ?>
