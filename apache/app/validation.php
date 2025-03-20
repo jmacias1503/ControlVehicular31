d600c41529407e45c5c9c7da30128c1a26ca3764
Alejandro Macias <alex.mac.fon@gmail.com>
2025-03-20 14:20:09 -0600

    feat: added validation layer

---
+++ apache/app/validation.php
@@ -0,0 +1,21 @@
+<?php
+  include("./controller.php");
+  $username=$_POST['username'];
+  $password=$_POST['password'];
+
+  $SQL="SELECT * FROM usuarios WHERE username='$username' AND password='$password' AND status='1' and block='0';";
+  $conn=connect();
+  $resultSet=execute($conn, $SQL);
+  $num_rows=mysqli_num_rows($resultSet);
+  $row=mysqli_fetch_row($resultSet);
+  if($num_rows == 0){
+    print("Usuario no existente");
+  } else {
+    print("Usuario valido. Entrando como ");
+    if(row[2] == 'A'){
+      print("administrador");
+    } else if(row[2] == 'U'){
+      print("usuario");
+    }
+  }
+?>
